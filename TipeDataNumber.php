605e0904a50ee5b804b1cc666a8fa5d2e619c159
idalaela <laelaida12xda@gmail.com>
2024-07-10 13:26:16 +0700

    bagian 2

---
+++ TipeDataNumber.php
@@ -0,0 +1,29 @@
+<?php
+    echo "Decimal ";
+    var_dump(1234);
+
+    echo "Octal ";
+    var_dump(01234);
+
+    echo "Hexadecimal ";
+    var_dump(0x1A);
+
+    echo "Binary ";
+    var_dump(0b111111);
+
+    echo"Underscore in number ";
+    var_dump(1_241_241_241);
+
+    // floating point
+    echo "Floating Point : ";
+    var_dump(1.234);
+
+    echo "Floating Point with E notation Plus (1.2 x 1000) : ";
+    var_dump(1.2e3);
+
+    echo "Floating Point with E notation Minus (7 x 0.001) : ";
+    var_dump(7e-3);
+
+    echo "Underscore di Floating Point : ";
+    var_dump(1_234.567);
+?>
